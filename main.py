b72db3f9fb368cd8658ceee2fe60fd3dad73b70b
diyaanna06 <diyavarghese20june@gmail.com>
2025-03-26 14:35:55 +0530

    added quiz to the home page

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, jsonify
+from flask import Flask,render_template, request, jsonify
 from flask_cors import CORS
 import pandas as pd
+import random
+import traceback
 import os
 import json
 import google.generativeai as genai
@@ -122,7 +124,73 @@ def get_recommendations():
 
     return jsonify(top_programs)
 
+#flask for quiz
+answers_list = [] 
+selected_questions=[]
+questions=[]
+num_questions=0
+df = pd.read_csv("questions.csv")
+@app.route('/')
+def home():
+    return render_template('index.html')
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')
+
+@app.route('/main')
+def main():
+    global num_questions
+    num_questions = request.args.get('num', type=int)
+    return render_template('main.html', num_questions=num_questions)
+
+@app.route('/get-questions', methods=["GET"])
+def get_questions():
+    global answers_list
+    global num_questions
+    global questions
+    questions=[]
+    n=num_questions
+    selected_questions = df.sample(n=n).to_dict(orient='records')
+    for i in range(n):
+        questions.append(selected_questions[i]['Question'])
+    answers = {str(q["ID"]): q["Correct Answer"] for q in selected_questions}
+    answers_list = list(answers.values())
+    return jsonify(selected_questions)
+
+@app.route("/submit-quiz", methods=["POST"])
+def submit_quiz():
+    try:
+        global answers_list
+        global questions
+        data = request.json
+        user_answers = data.get("answers", {})
+        user_answers_values = list(user_answers.values())
+        total = len(answers_list)
+        score=0
+        for i in range(total):
+            if (user_answers_values[i]==answers_list[i]):
+                score+=1
+        return jsonify({
+            "score": score,
+            "total": total,
+            "questions": questions,
+            "correct_answers": answers_list ,
+            "user_answers": [user_answers[f"q{i}"] for i in range(total)],
+        })
+    except Exception as e:
+        print(traceback.format_exc())
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/result')
+def result():
+    final_score = request.args.get("score", type=int)
+    total_questions = request.args.get("total", type=int)
+    return render_template("result.html", score=final_score, total=total_questions)
 # ========================== RUN FLASK APP ==========================
 
+""" quiz flask running
+if __name__ == "__main__":
+    app.run(debug=True)
+      """
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
