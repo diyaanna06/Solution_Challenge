0880b73ed92cfd85f3a4110e5336b02c35029e67
merintheres <merintheresjose@gmail.com>
2025-04-06 20:36:30 +0530

    added about.html and connected it with home.html

--- main.py
+++ main.py
@@ -276,5 +276,9 @@ def get_salary():
 def services():
     return render_template("services.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
